8c3cda6465d90d1c94cef5d7ed94314f27335d90
sai-kawamura <59859958+sai-kawamura@users.noreply.github.com>
2025-03-27 19:17:53 +0900

    wip

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/fastlp' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/fastlp/' : '',
 };
 
 export default nextConfig;
